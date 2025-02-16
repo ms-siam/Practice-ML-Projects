3b23c81f59cc8f37ad82ace368fd02f6330a5d46
ms-siam <mobaroksiam10@gmail.com>
2025-02-16 14:51:41 +0600

    storing cross val score to cv

--- The_iris_dataset/iris_classfication.py
+++ The_iris_dataset/iris_classfication.py
@@ -60,4 +60,6 @@ models.append(('SVM', SVC(gamma='auto')))
 results = []
 names = []
 for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
     
