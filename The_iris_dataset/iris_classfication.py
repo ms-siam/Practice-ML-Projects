3a51f2dda61aa155599e4c5c2b223f05d74c5f96
ms-siam <mobaroksiam10@gmail.com>
2025-02-16 13:55:38 +0600

    imported LogisticRegression from sklearn

--- The_iris_dataset/iris_classfication.py
+++ The_iris_dataset/iris_classfication.py
@@ -8,3 +8,5 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+
