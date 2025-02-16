049aba0c057773e979c584e432328b2f1692494d
ms-siam <mobaroksiam10@gmail.com>
2025-02-16 14:53:27 +0600

    Appending model name to names

--- The_iris_dataset/iris_classfication.py
+++ The_iris_dataset/iris_classfication.py
@@ -63,4 +63,5 @@ for name, model in models:
     kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
     cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
     results.append(cv_results)
+    names.append(name)
     
