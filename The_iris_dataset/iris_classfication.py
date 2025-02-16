7c82ff831ae65ecdd9b6086583608efe2c11334b
ms-siam <mobaroksiam10@gmail.com>
2025-02-16 13:54:50 +0600

    imported confusion_matrix from sklearn

--- The_iris_dataset/iris_classfication.py
+++ The_iris_dataset/iris_classfication.py
@@ -6,4 +6,5 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
