664cd0cde61ac34afeff43d80a1c5dc27b19c481
ms-siam <mobaroksiam10@gmail.com>
2025-02-16 14:16:05 +0600

    Comment out the summary of the dataset

--- The_iris_dataset/iris_classfication.py
+++ The_iris_dataset/iris_classfication.py
@@ -21,8 +21,9 @@ from sklearn.svm import SVC
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
-
+'''
 print(dataset.shape)
 print(dataset.head(20))
 print(dataset.describe())
 print(dataset.groupby('class').size())
+'''
