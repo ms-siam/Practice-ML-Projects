e2677e7bea6fab96f405ccdd1943504746112fd9
ms-siam <mobaroksiam10@gmail.com>
2025-02-16 13:53:48 +0600

    imported StratifiedKFold from sklearn

--- The_iris_dataset/iris_classfication.py
+++ The_iris_dataset/iris_classfication.py
@@ -4,3 +4,5 @@ from pandas.plotting import scatter_matrix
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+
