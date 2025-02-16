c93f1d891b43db3484e9ec99b3d2efdcbd2a6a27
ms-siam <mobaroksiam10@gmail.com>
2025-02-16 14:39:34 +0600

    Created a empty list 'models'

--- The_iris_dataset/iris_classfication.py
+++ The_iris_dataset/iris_classfication.py
@@ -45,3 +45,7 @@ array = dataset.values
 X = array[:,0:4]
 y = array[:,4]
 X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+#Spot Check Algorithms
+
+models = []
