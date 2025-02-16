50d69e14fe421a373f9187ac66b58a5df2ef4e76
ms-siam <mobaroksiam10@gmail.com>
2025-02-16 14:07:45 +0600

    checking the datasets shape

--- The_iris_dataset/iris_classfication.py
+++ The_iris_dataset/iris_classfication.py
@@ -22,3 +22,4 @@ url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
 
+print(dataset.shape)
