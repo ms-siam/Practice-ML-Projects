890fa6f4257fc6a97df02d0a724e993856e72d2b
ms-siam <mobaroksiam10@gmail.com>
2025-02-16 13:53:10 +0600

    Imported cross_val_score from sklearn

--- The_iris_dataset/iris_classfication.py
+++ The_iris_dataset/iris_classfication.py
@@ -3,4 +3,4 @@ from pandas import read_csv
 from pandas.plotting import scatter_matrix
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
-
+from sklearn.model_selection import cross_val_score
